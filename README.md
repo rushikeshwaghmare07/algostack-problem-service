255d8c83dd487da445e914d3c0df84c27a79be86
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-07-24 23:21:42 +0530

    Feat: Added routing structure for versioned api for problems

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Algostack Problem Setting Service
+
+---
+
+## How routing is working in the project
+
+- /api/v1/problems/ping
+
+  - because the routes starts with /api
+
+    /api -> /v1 -> /problems -> /ping
+
+    apiRouter -> v1Router -> problemRouter -> problemController -> service layer
---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,18 @@
+function pingProblemController(req, res) {
+  return res.json({ message: "Problem controller is up" });
+}
+
+function addProblem(req, res) {}
+function getProblem(req, res) {}
+function getProblems(req, res) {}
+function deleteProblem(req, res) {}
+function updateProblem(req, res) {}
+
+module.exports = {
+  pingProblemController,
+  addProblem,
+  getProblem,
+  getProblems,
+  deleteProblem,
+  updateProblem,
+};
