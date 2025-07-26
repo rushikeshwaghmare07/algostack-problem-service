cc63842236f966bcf4777e3789c3cf9bdcd083ca
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-07-27 01:06:23 +0530

    Feat: prepared schema for problems

---
+++ src/models/problem.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const problemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title cannot be empty"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description cannot be empty"],
+  },
+  difficulty: {
+    type: String,
+    enum: {
+      values: ["easy", "medium", "hard"],
+      message: "{VALUE} is not supported",
+    },
+    required: [true, "Difficulty cannot be empty"],
+    default: "easy",
+  },
+  testCases: [
+    {
+      input: {
+        type: String,
+        required: true,
+      },
+      output: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  editorial: {
+    type: String,
+  },
+});
+
+const Problem = mongoose.model("Problem", problemSchema);
+
+module.exports = Problem;
